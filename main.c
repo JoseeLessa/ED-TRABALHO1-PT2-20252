f516d4efddb41203702ace1f2cda85bc6313c135
JoseeLessa <145026909+JoseeLessa@users.noreply.github.com>
2025-12-08 07:07:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main() {
     BDMatches *bdm = bdmatches_create();
     bdpartidas_load_csv(bdm, "partidas.txt");
     
+    bdpartidas_increase_in_team(bdm, bdt);
     
     char opcao;
     do {
@@ -20,16 +21,15 @@ int main() {
         show_menu();
         scanf(" %c", &opcao);
         
-        bdpartidas_increase_in_team(bdm, bdt);
-        
         switch (opcao) 
         {
             case '1': option_consult_team(bdt); break;
-        //    case '2': opcao_consultar_partidas(bdp, bdt); break;
-        //    case '3': opcao_atualizar_partida(bdp, bdt); break;
-        //    case '4': opcao_remover_partida(bdp, bdt); break;
+            case '2': int a = option_consult_match(bdm, bdt); break;
+            case '3': option_update_match(bdm, bdt); break;
+            case '4': option_remove_match(bdm, bdt); break;
             case '5': option_insert_match(bdm, bdt); break;
             case '6': option_print_classification(bdt); break;
+            default: printf("Opção inválida, tente novamente.\n"); break;
         }
         
         pause_screen();
